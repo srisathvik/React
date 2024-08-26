e33ef308f5d84463d2eef7d4971fab4e5fbac1d6
srisathvik <komminenisathvik@gmail.com>
2024-08-26 18:34:07 +0530

    styling

--- udemy/04-styling-project/01-starting-project/src/components/AuthInputs.jsx
+++ udemy/04-styling-project/01-starting-project/src/components/AuthInputs.jsx
@@ -1,15 +1,6 @@
 import { useState } from 'react';
-import {styled} from "styled-components";
 import Button from './Button';
 import Input from './Input';
-const ControlledInputs = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`
-
-
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -32,25 +23,25 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlledInputs>
+    <div id="auth-inputs" className='w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800'>
+      <div className='flex flex-col gap-2 mb-6'>
           <Input
             label="email"
             type="email"
-            $invalid={emailNotValid}
+            invalid={emailNotValid}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
          <Input
             label="password"
             type="password"
-            $invalid={passwordNotValid}
+            invalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
           />
-      </ControlledInputs>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
--- udemy/04-styling-project/01-starting-project/tailwind.config.js
+++ udemy/04-styling-project/01-starting-project/tailwind.config.js
@@ -6,7 +6,11 @@ export default {
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        title:['"pacifio"', 'cursive']
+      }
+    },
   },
   plugins: [],
 }
