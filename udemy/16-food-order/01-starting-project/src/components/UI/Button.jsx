6bb98255ab99e77659c2e65d66c8eb5e710038b3
srisathvik <komminenisathvik@gmail.com>
2024-10-27 22:38:27 +0530

    initial commit

---
+++ udemy/16-food-order/01-starting-project/src/components/UI/Button.jsx
@@ -0,0 +1,8 @@
+export default function Button({children, textOnly, classes, ...props}){
+    let cssClasses = textOnly ? 'text-button': 'button';
+    cssClasses += ' ' + classes;
+
+    return(
+        <button className={cssClasses} {...props} >{children}</button>
+    )
+}
